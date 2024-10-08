6b6b5ed33240806cee1b8ff0ff2fbda80f32919e
jrest01 <julian11072000@hotmail.com>
2024-10-07 20:31:06 -0500

    Not null avaibility

--- Models/Attendance.cs
+++ Models/Attendance.cs
@@ -4,6 +4,6 @@ public class Attendance
     public int UserId { get; set; }
     public int MeetId { get; set; }
 
-    public User User { get; set; }
-    public Meet Meet { get; set; }
+    public User? User { get; set; }
+    public Meet? Meet { get; set; }
 }
